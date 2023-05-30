da1fbddc17b70e423dc3710dfd78fa360479fd10
MariceAshtiani <marice95@hotmail.no>
2023-05-30 15:16:59 +0200

    Added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import Header from "./components/ui/Header";
-import Footer from "./components/ui/Footer";
+import { Routes, Route } from "react-router-dom"; 
 
 export default function App() {
   return (
-    <div>
-      <Header>
-        <h1>eCom logo</h1>
-      </Header>
-
-      <main>
-        <h2>Site Content</h2>
-      </main>
-
-      <Footer>
-        Copyright {(new Date()).getFullYear()}
-      </Footer>
-    </div>
+<Routes>
+  
+</Routes>
   )
 }
 

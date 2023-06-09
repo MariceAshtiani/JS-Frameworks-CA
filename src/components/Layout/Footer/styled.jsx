3f3df55572930ae8d6a19b0b088e0a046df72e22
MariceAshtiani <marice95@hotmail.no>
2023-06-09 20:16:39 +0200

    Created a simple footer

---
+++ src/components/Layout/Footer/styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default styled.footer`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    padding: 50px 0px;
+    margin: 0 auto;
+    margin-top: 3rem;
+    color: black;
+
+`;

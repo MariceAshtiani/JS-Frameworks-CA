3db22f1f3a4f535e60a57c5f5502682a7284247f
MariceAshtiani <marice95@hotmail.no>
2023-06-09 20:18:11 +0200

    fixed and styled cart-icon for navbar

---
+++ src/components/Layout/Header/Navbar/Carticon/styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.div`
+    position: relative;
+    cursor: pointer;
+
+    span {
+        position: absolute;
+        font-size: 1rem;
+        color: orange;
+        padding: 0 5px;
+        left: 20px;
+    }
+`;

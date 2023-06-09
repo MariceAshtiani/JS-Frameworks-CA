0a5253cbf9d44b4287e97859036cc61c7d7e4534
MariceAshtiani <marice95@hotmail.no>
2023-06-09 21:26:56 +0200

    created and styled contact-page and form

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,3 +1,23 @@
-export default function Contact() {
-    return <div>Contact</div>
-}
+import ContactForm from "../../components/ui/Form/ContactForm";
+import StyledHeadline from "./styled";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+export default function ContactPage() {
+    return (
+        <>
+        <HelmetProvider>
+            <Helmet>
+                <title>TrendCart | Contact</title>
+            </Helmet>
+        </HelmetProvider>
+        <main>
+            <StyledHeadline>
+                <h1>Contact</h1> 
+            </StyledHeadline>
+            <div className="mainContainer">
+                <ContactForm />
+            </div>
+        </main>
+        </>
+    );
+};

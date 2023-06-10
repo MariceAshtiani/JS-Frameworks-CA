dc6608aa2f16871ccd4b72194cc6c2a779a29e92
MariceAshtiani <marice95@hotmail.no>
2023-06-10 23:58:51 +0200

    checkoutpage

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -1,3 +1,37 @@
-export default function Checkout() {
-    return <div>Checkout</div>
-}
+import CheckoutForm from "../../components/ui/Form/CheckoutForm";
+import Cart from "../../components/ui/Cart";
+import StyledHeader from "./styled";
+
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+export default function CheckoutPage() {
+    return (
+        <>
+        <HelmetProvider>
+            <Helmet>
+                <title>TrendCart | Checkout</title>
+            </Helmet>
+        </HelmetProvider>
+        <main>
+            <StyledHeader>
+                <h1>Checkout</h1>
+            </StyledHeader>
+            <div className="mainContainer">
+                <section>
+                    <StyledHeader>
+                        <h2>Order Confirmation</h2>
+                    </StyledHeader>
+                    
+                    <Cart />
+                </section>
+                <section>
+                <StyledHeader>
+                        <h2>Payment details</h2>
+                    </StyledHeader>
+                    <CheckoutForm />
+                </section>
+            </div>
+        </main>
+        </>
+    );
+};

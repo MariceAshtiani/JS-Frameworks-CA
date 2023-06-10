9d5adbbc7d47dabb05ea36636898cbbd84998c00
MariceAshtiani <marice95@hotmail.no>
2023-06-10 22:53:41 +0200

    cart-page

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,3 +1,19 @@
-export default function Cart() {
-    return <div>Cart</div>
-}
+import Cart from "../../components/ui/Cart";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+
+export default function CartPage() {
+    return(
+        <>
+        <HelmetProvider>
+            <Helmet>
+                <title>TrendCart | Cart</title>
+            </Helmet>
+        </HelmetProvider>
+        <main>
+            <h1>View Cart</h1>
+            <Cart />
+        </main>
+        </>
+    );
+};

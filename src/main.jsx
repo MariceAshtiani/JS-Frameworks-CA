34cb8681b7cadd42eb703e1653fa70c4bdb9cb15
MariceAshtiani <marice95@hotmail.no>
2023-06-11 00:08:29 +0200

    added the context, theme and globalstyle to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Theme from "./styles/Theme.jsx";
+import GlobalStyle from "./styles/GlobalStyles.jsx";
 import { BrowserRouter } from 'react-router-dom'
+import { Context } from "./components/Context";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <Context>
+        <Theme>
+          <GlobalStyle />
+          <App />
+        </Theme>
+      </Context>
     </BrowserRouter>
   </React.StrictMode>,
 )
